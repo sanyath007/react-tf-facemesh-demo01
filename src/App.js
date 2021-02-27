9efe75c02fdd5fcacc0f76545b4362a17d9a225d
sanyath007 <sanyath007@gmail.com>
2021-02-27 08:22:59 +0700

    Import dependencies and setup webcam and canvas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+/**
+ * Work flow
+ * - install dependencies
+ * - import dependencies
+ * - setup webcam and canvas
+ * - define references to those
+ */
+import React, { useRef } from 'react';
+import * as tf from '@tensorflow/tfjs';
+import * as facemesh from '@tensorflow-models/facemesh';
+import Webcam from 'react-webcam';
 import './App.css';
 
 function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+  const videoConstraints = {
+    width: 1280,
+    height: 720,
+    facingMode: 'user'
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Webcam
+          ref={webcamRef}
+          audio={false}
+          screenshotFormat="image/jpeg"
+          videoConstraints={videoConstraints}
+          style={
+            {
+              position: 'absolute',
+              marginLeft: 'auto',
+              marginRight: 'auto',
+              left: 0,
+              right: 0,
+              zIndex: 9,
+              width: 640,
+              height: 480,
+            }
+          }
+        />
+
+        <canvas
+          ref={canvasRef}
+          style={
+            {
+              position: 'absolute',
+              marginLeft: 'auto',
+              marginRight: 'auto',
+              left: 0,
+              right: 0,
+              zIndex: 9,
+              width: 640,
+              height: 480,
+            }
+          }
+        />
       </header>
     </div>
   );
 }
 
 export default App;
+
+
+
