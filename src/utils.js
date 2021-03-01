d91a046c517930aeb39d9f29c7b8aa781ac0909d
sanyath007 <sanyath007@gmail.com>
2021-03-01 14:16:55 +0700

    Complete with create drawPath in utils.js and use its in drawMesh function

--- src/utils.js
+++ src/utils.js
@@ -2641,13 +2641,42 @@ export const TRIANGULATION = [
     448,
     255,
 ];
+
 // Draw triangle
+const drawPath = (ctx, points, closePath) => {
+    const region = new Path2D();
+    region.moveTo(points[0][0], points[0][1]);
+    for (let i = 0; i < points.length; i++) {
+        const point = points[i];
+        region.lineTo(point[0], point[1]);
+    }
+
+    if (closePath) {
+        region.closePath();
+    }
+
+    ctx.strokeStyle = 'pink';
+    ctx.stroke(region);
+};
+
 // Draw the point
 export const drawMesh = (predictions, ctx) => {
     if (predictions.length > 0) {
         predictions.forEach(prediction => {
             const keypoints = prediction.scaledMesh;
 
+            // Draw triangles
+            for (let i = 0; i < TRIANGULATION.length/3; i++) {
+                const points = [
+                    TRIANGULATION[i * 3],
+                    TRIANGULATION[i * 3 + 1],
+                    TRIANGULATION[i * 3 + 2],
+                ].map(index => keypoints[index]);
+
+                drawPath(ctx, points, true);
+            }
+
+            // Draw points
             for (let i = 0; i < keypoints.length; i++) {
                 const x = keypoints[i][0];
                 const y = keypoints[i][1];
